c01bd65105e476083268289e5ff40cd08bef5896
aaron <smitham50@gmail.com>
2025-04-03 16:07:40 -0400

    Add basic header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <header className="bg-slate-800 h-34 grid grid-cols-3 gap-4 text:white @container">
+      <p className="m-auto text-white text-2xl font-[Gowun_Dodum]">AMS Guitar Repair</p>
+    </header>
+  );
+}
+
+export default Header;
